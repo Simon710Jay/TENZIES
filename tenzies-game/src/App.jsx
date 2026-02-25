76c11bbeb9046efcf28e597920aa16f2c315c4ab
Simon Ejeh <simonjay710@gmail.com>
2026-02-25 19:26:43 +0100

    Initialize Tenzies game project with React, Vite, and ESLint setup; add main components, styles, and assets.

---
+++ tenzies-game/src/App.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react"
+import Die from "./components/Die"
+import { nanoid } from "nanoid"
+
+export default function App() {
+    const [dice, setDice] = useState(generateAllNewDice())
+
+    function generateAllNewDice() {
+        return new Array(10)
+            .fill(0)
+            .map(() => ({
+                value: Math.ceil(Math.random() * 6),
+                isHeld: false,
+                id: nanoid()
+            }))
+    }
+
+    function hold(id) {
+        setDice(prevDice => prevDice.map(die => {
+            return die.id === id ? { ...die, isHeld: !die.isHeld } : die
+        }))
+    }
+
+    function rollDice() {
+        setDice(prevDice => prevDice.map(die => {
+            return die.isHeld ? die : { ...die, value: Math.ceil(Math.random() * 6) }
+        }))
+    }
+
+
+
+    const diceElements = dice.map(dieObj =>
+        <Die
+            key={dieObj.id}
+            id={dieObj.id}
+            value={dieObj.value}
+            isHeld={dieObj.isHeld}
+            hold={hold}
+        />)
+
+    return (
+        <main>
+            <h1 className="tittle">Tenzies</h1>
+            <p className="instructions">Roll until all dice are the same. Click each die to freeze
+            it at its current value between rolls.</p>
+            <div className="dice-container">
+                {diceElements}
+            </div>
+
+            <button onClick={rollDice} className="dice">Roll Dice</button>
+
+        </main>
+    )
+}
